a25cd7ab079589b2f7dcd37b5067cf47a25e9284
Gordon <geniusgordon@gmail.com>
2017-02-14 22:05:09 +0800

    styled-components ssr workaround

--- html.js
+++ html.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { prefixLink } from 'gatsby-helpers';
 import { GoogleFont, TypographyStyle } from 'react-typography';
+import styleSheet from 'styled-components/lib/models/StyleSheet';
 import typography from './utils/typography';
 
 const BUILD_TIME = new Date().getTime();
+styleSheet.flush();
 
 module.exports = React.createClass({
   displayName: 'HTML',
@@ -14,18 +16,12 @@ module.exports = React.createClass({
   render() {
     const { body } = this.props;
     const head = Helmet.rewind();
-
     let css;
     if (process.env.NODE_ENV === 'production') {
-      css = (
-        <style
-          dangerouslySetInnerHTML={{
-            __html: require('!raw!./public/styles.css'),
-          }}
-        />
-      );
+      const styles = require('!raw!./public/styles.css') +
+        styleSheet.rules().map(rule => rule.cssText).join('\n');
+      css = <style dangerouslySetInnerHTML={{ __html: styles }} />;
     }
-
     return (
       <html lang="en">
         <head>
@@ -42,10 +38,7 @@ module.exports = React.createClass({
           {css}
         </head>
         <body>
-          <div
-            id="react-mount"
-            dangerouslySetInnerHTML={{ __html: body }}
-          />
+          <div id="react-mount" dangerouslySetInnerHTML={{ __html: body }} />
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
         </body>
       </html>
